e13fea7ed9859bf9d8760d99797f68c61adbac9e
samogera <samuelohillary@gmail.com>
2024-05-07 10:38:50 +0300

    final changes

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,7 +1,7 @@
-import React, { Component } from "react"
-import BotCollection from './BotCollection'
-import BotArmy from './YourBotArmy'
-import BotSpecs from '../components/BotSpecs'
+import React, { Component } from "react";
+import BotCollection from "./BotCollection";
+import BotArmy from "./YourBotArmy";
+import BotSpecs from "../components/BotSpecs";
 
 class BotsPage extends Component {
   state = {
@@ -10,79 +10,94 @@ class BotsPage extends Component {
     botArmy: [],
     collectionVisible: true,
     botSpecs: {},
-  }
+  };
 
   componentDidMount() {
-    fetch('https://asher-arebaa.github.io/botdb/db.json')
-      .then(response => response.json())
-      .then(bots => this.setState({ botCollection: bots, filteredCollection: bots }))
-      .then(console.log("Bots Fetched!"))
+    fetch("https://db-json-e59m.onrender.com/bots")
+      .then((response) => response.json())
+      .then((bots) =>
+        this.setState({ botCollection: bots, filteredCollection: bots })
+      )
+      .then(() => console.log("Bots Fetched!"))
+      .catch((error) => console.error("Error fetching bots:", error));
   }
 
   addToArmy = (bot) => {
-    const newCollection = this.state.filteredCollection.filter(card => card.bot_class !== bot.bot_class)
+    const newCollection = this.state.filteredCollection.filter(
+      (card) => card.bot_class !== bot.bot_class
+    );
     this.setState({
       filteredCollection: newCollection,
       botArmy: [...this.state.botArmy, bot],
       collectionVisible: true,
-    })
-  }
+    });
+  };
 
   removeFromArmy = (bot) => {
-    const newArmy = this.state.botArmy.filter(card => card.id !== bot.id)
-    const armyClasses = newArmy.map(bot => bot.bot_class)
-    const newCollection = this.state.botCollection.filter(bot => {
-      console.log("Filter:", !armyClasses.includes(bot.bot_class))
-      return !armyClasses.includes(bot.bot_class)
-    })
-    console.log("newCollection", newCollection)
+    const newArmy = this.state.botArmy.filter((card) => card.id !== bot.id);
+    const armyClasses = newArmy.map((bot) => bot.bot_class);
+    const newCollection = this.state.botCollection.filter((bot) => {
+      console.log("Filter:", !armyClasses.includes(bot.bot_class));
+      return !armyClasses.includes(bot.bot_class);
+    });
+    console.log("newCollection", newCollection);
 
-    this.setState({ botArmy: newArmy, filteredCollection: newCollection })
-  }
+    this.setState({ botArmy: newArmy, filteredCollection: newCollection });
+  };
 
   removeBotPermanently = (bot) => {
-    let newCollection = this.state.botCollection.filter(card => card !== bot)
-    let newFilteredCollection = this.state.filteredCollection.filter(card => card !== bot)
-    let newArmy = this.state.botArmy.filter(card => card !== bot)
+    let newCollection = this.state.botCollection.filter(
+      (card) => card !== bot
+    );
+    let newFilteredCollection = this.state.filteredCollection.filter(
+      (card) => card !== bot
+    );
+    let newArmy = this.state.botArmy.filter((card) => card !== bot);
 
-    this.setState({ botCollection: newCollection, filteredCollection: newFilteredCollection, botArmy: newArmy })
+    this.setState({
+      botCollection: newCollection,
+      filteredCollection: newFilteredCollection,
+      botArmy: newArmy,
+    });
 
-    fetch(`https://asher-arebaa.github.io/botdb/db.json${bot.id}`, {
-      method: 'DELETE'
-    }).then(response => response.json())
-      .then(result => console.log(result))
-  }
+    fetch(`https://db-json-e59m.onrender.com/bots/${bot.id}`, {
+      method: "DELETE",
+    })
+      .then((response) => response.json())
+      .then((result) => console.log(result));
+  };
 
   displayBotSpecs = (bot) => {
-    this.setState({ collectionVisible: false, botSpecs: bot })
-  }
+    this.setState({ collectionVisible: false, botSpecs: bot });
+  };
 
   displayBotCollection = () => {
-    this.setState({ collectionVisible: true })
-  }
+    this.setState({ collectionVisible: true });
+  };
 
   render() {
-    const { filteredCollection, botArmy, botSpecs, collectionVisible } = this.state
+    const { filteredCollection, botArmy, botSpecs, collectionVisible } =
+      this.state;
 
     return (
       <div>
         <BotArmy
           bots={botArmy}
           action={this.removeFromArmy}
-          removeCard={this.removeBotPermanently} />
-        {collectionVisible
-          ? < BotCollection
+          removeCard={this.removeBotPermanently}
+        />
+        {collectionVisible ? (
+          <BotCollection
             botCollection={filteredCollection}
             action={this.displayBotSpecs}
-            removeCard={this.removeBotPermanently} />
-          : < BotSpecs
-            bot={botSpecs}
-            back={this.displayBotCollection}
-            enlist={this.addToArmy} />
-        }
+            removeCard={this.removeBotPermanently}
+          />
+        ) : (
+          <BotSpecs bot={botSpecs} back={this.displayBotCollection} enlist={this.addToArmy} />
+        )}
       </div>
-    )
+    );
   }
 }
 
-export default BotsPage
+export default BotsPage;
